137443620c040ab1d3c39ae72a22da9941b17a13
Nikhil Jain <jainnikhil472@gmail.com>
2023-10-12 08:19:01 +0530

    Finish the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    let interval;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setCounter((prevCounter) => prevCounter + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isRunning]);
+
+  const handleStartStop = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setCounter(0);
+  };
+
   return (
     <div>
-      Hello
+      <h1>Timer: {counter} seconds</h1>
+      <button onClick={handleStartStop}>{isRunning ? "Stop" : "Start"}</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
